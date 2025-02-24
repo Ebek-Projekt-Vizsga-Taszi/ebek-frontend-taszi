0cddd7351722796c09badab9c692908f245bf9c8
Andorr-1 <benandor4@gmail.com>
2025-02-24 20:48:08 +0100

    buttonsend

--- src/components/navbarcomponents/steps/Step3.jsx
+++ src/components/navbarcomponents/steps/Step3.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import SendIcon from "@mui/icons-material/Send";
+import Button from '@mui/material/Button';
 
 const Step3 = ({ handleNext, handleBack }) => {
   return (
@@ -6,70 +8,33 @@ const Step3 = ({ handleNext, handleBack }) => {
       <h2 className="text-2xl font-bold mb-4">Veszettség elleni oltás utolsó adatai</h2>
       <form>
         <div className="mb-4">
-          <label className="block text-white font-newsreader mb-2">
-            Utolsó védőoltás időpontja:
-          </label>
-          <input
-            className="border border-gray-300 p-2 w-full text-white bg-grey"
-            type="date"
-          />
+          <label className="block text-white font-newsreader mb-2">Utolsó védőoltás időpontja:</label>
+          <input className="border border-gray-300 p-2 w-full text-white bg-grey" type="date" />
         </div>
         <div className="mb-4">
-          <label className="block text-white font-newsreader mb-2">
-            Orvosi bélyegző szám:
-          </label>
-          <input
-            className="border border-gray-300 p-2 w-full text-white bg-grey"
-            type="text"
-            placeholder="XXX-XXX-XXX"
-          />
+          <label className="block text-white font-newsreader mb-2">Orvosi bélyegző szám:</label>
+          <input className="border border-gray-300 p-2 w-full text-white bg-grey" type="text" placeholder="XXX-XXX-XXX" />
         </div>
         <div className="mb-4">
-          <label className="block text-white font-newsreader mb-2">
-            Oltóanyag sorszáma:
-          </label>
-          <input
-            className="border border-gray-300 p-2 w-full text-white bg-grey"
-            type="text"
-            placeholder="XXX-XXX-XXX"
-          />
+          <label className="block text-white font-newsreader mb-2">Oltóanyag sorszáma:</label>
+          <input className="border border-gray-300 p-2 w-full text-white bg-grey" type="text" placeholder="XXX-XXX-XXX" />
         </div>
         <h2 className="text-2xl font-bold mb-4">Oltási kiskönyv adatok</h2>
         <div className="mb-4">
-          <label className="block text-white font-newsreader mb-2">
-            Oltási könyv szám:
-          </label>
-          <input
-            className="border border-gray-300 p-2 w-full text-white bg-grey"
-            type="text"
-            placeholder="AA123456789"
-          />
+          <label className="block text-white font-newsreader mb-2">Oltási könyv szám:</label>
+          <input className="border border-gray-300 p-2 w-full text-white bg-grey" type="text" placeholder="AA123456789" />
         </div>
         <div className="mb-4">
-          <label className="block text-white font-newsreader mb-2">
-            Állatorvos bélyegző száma:
-          </label>
-          <input
-            className="border border-gray-300 p-2 w-full text-white bg-grey"
-            type="text"
-            placeholder="XXX-XXX-XXX"
-          />
+          <label className="block text-white font-newsreader mb-2">Állatorvos bélyegző száma:</label>
+          <input className="border border-gray-300 p-2 w-full text-white bg-grey" type="text" placeholder="XXX-XXX-XXX" />
         </div>
-        <div className="flex justify-between">
-          <button
-            type="button"
-            onClick={handleBack}
-            className="bg-gray-500 text-white py-1 px-3 rounded"
-          >
+        <div className="flex justify-between mb-4">
+          <button type="button" onClick={handleBack} className="bg-gray-500 text-white py-1 px-3 rounded">
             Vissza
           </button>
-          <button
-            type="button"
-            onClick={handleNext}
-            className="bg-blue-500 text-white py-1 px-3 rounded"
-          >
-            Tovább
-          </button>
+          <Button variant="contained" endIcon={<SendIcon />} onClick={handleNext}>
+            Küldés
+          </Button>
         </div>
       </form>
     </div>
